7fdc52b84e116407263ea6701154089cefa14075
AmirMohammad Safari <88091064+Osb0rn3@users.noreply.github.com>
2023-04-26 21:42:29 +0330

    Implementing BugCrowd rate limit handling mechanism

--- config.py
+++ config.py
@@ -1,16 +1,31 @@
-from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+import asyncio
 import logging
 import httpx
 import json
+from tenacity import (retry, stop_after_attempt, wait_random_exponential, before_log,
+                      retry_if_exception_type, after_log)
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class API:
-    def __init__(self, base_url: str) -> None:
+    def __init__(self, base_url: str, rate_limit: float = 0.5) -> None:
         self.base_url = base_url
         self.session = httpx.AsyncClient()
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.rate_limit = rate_limit
 
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(5), retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError, json.JSONDecodeError)))
+    async def _wait(self):
+        await asyncio.sleep(self.rate_limit)
+
+    @retry(
+        stop=stop_after_attempt(5),
+        wait=wait_random_exponential(multiplier=1, max=60),
+        retry=retry_if_exception_type(
+            (httpx.RequestError, httpx.HTTPStatusError, json.JSONDecodeError)),
+        after=after_log(logger, logging.WARNING)
+    )
     async def get(self, endpoint: str, params: dict = None) -> dict:
         """
         Send an HTTP GET request to the specified API endpoint and return the response as a dictionary.
@@ -25,6 +40,7 @@ class API:
         try:
             response = await self.session.get(endpoint, params=params, timeout=60.0)
             response.raise_for_status()
+            await self._wait()
             return response.json()
         except httpx.RequestError as e:
             self.logger.error(f"Error: {e}, Endpoint: {endpoint}")
